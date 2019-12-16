63a6479d8eb34b04c391e6f4f492a4dedaae4566
Beau Batchelor <beauwbatchelor@gmail.com>
2019-12-15 21:17:33 -0800

    Adding all templates and first draft of layout

--- app.py
+++ app.py
@@ -7,5 +7,25 @@ def index():
     
     return render_template("index.html")
 
+@app.route("/home")
+def home():
+    
+    return render_template("index.html")
+
+@app.route("/bio")
+def bio():
+    
+    return render_template("bio.html")
+
+@app.route("/services")
+def services():
+    
+    return render_template("services.html")
+
+@app.route("/contact")
+def contact():
+    
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
