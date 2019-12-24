0581896433315dbe8e9dbdaacd10ffcdcf81e71d
Beau Batchelor <beauwbatchelor@gmail.com>
2019-12-23 19:27:44 -0800

    changeing config from local to heroku vault

--- tables.py
+++ tables.py
@@ -2,13 +2,13 @@ from __init__ import app
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config_local import sql_user, sql_pass
-from datetime import datetime
-# from config import (sql_url)
+# from datetime import datetime
+from config import (sql_url)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{sql_user}:{sql_pass}@ofcmikjy9x4lroa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/o5hjwzcvjuw3utaz'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_DATABASE_URI'] = f'{sql_url}'
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{sql_user}:{sql_pass}@ofcmikjy9x4lroa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/o5hjwzcvjuw3utaz'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = f'{sql_url}'
 db = SQLAlchemy(app)
 
 
