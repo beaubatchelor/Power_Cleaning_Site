a6b0224aa4386bd2d4f9f66420fdf1164ecd28d6
Beau Batchelor <beauwbatchelor@gmail.com>
2019-12-25 11:59:36 -0800

    adding title to bio and calendar table

--- tables.py
+++ tables.py
@@ -1,13 +1,11 @@
 from __init__ import app
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from config_local import sql_user, sql_pass
+from config_local import (sql_url)
 from datetime import datetime
-from config import (sql_url)
+# from config import (sql_url)
 
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{sql_user}:{sql_pass}@ofcmikjy9x4lroa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/o5hjwzcvjuw3utaz'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = f'{sql_url}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -26,3 +24,12 @@ class Customer_site_dump(db.Model):
 
     def __repr__(self):
         return '<Customer_site_dump %r>' % self.email_address
+
+class Calendar_table(db.Model):
+    event_id = db.Column(db.Integer, primary_key=True)
+    event_date = db.Column(db.DateTime, nullable=False)
+    event_time = db.Column(db.DateTime, nullable=False)
+    event_hour = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Calendar_table %r>' % self.event_id
